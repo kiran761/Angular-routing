9bdcb81a5571d6d0e1ee5986b6b82bab4a33fbc4
kiran <rkirankumar761@gmail.com>
2024-08-21 14:58:42 +0530

    Routing

---
+++ src/app/app.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'routing';
+
+  constructor(private router : Router){}
+
+  renderHome(home:string){
+    this.router.navigate([home]);
+  }
+
+  renderAboutus(aboutus:string){
+    this.router.navigate([aboutus]);
+  }
+
+  renderRegister(register:string){
+    this.router.navigate([register]);
+  }
+}
